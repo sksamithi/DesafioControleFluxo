5eef70dcad2f853112524583b7dc8578eaca890a
sksamithi <vieira.thaism@gmail.com>
2024-07-02 17:19:19 -0300

    Desafio

--- src/Contador.java
+++ src/Contador.java
@@ -1,29 +1,33 @@
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Contador {
+    
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite o primeiro parâmetro: ");
-        int parametroUm = scanner.nextInt();
-
-        System.out.println("Digite o segundo parâmetro: ");
-        int parametroDois = scanner.nextInt();
-
         try {
-            contar(parametroUm, parametroDois);
-        }catch (ParametrosInvalidosException exception){
-            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+
+            System.out.println("Digite o primeiro parâmetro: ");
+            int parametroUm = scanner.nextInt();
+
+            System.out.println("Digite o segundo parâmetro: ");
+            int parametroDois = scanner.nextInt();
+
+            try {
+                contar(parametroUm, parametroDois);
+            }catch (ParametrosInvalidosException e){
+                System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+            }
+        }catch (InputMismatchException e) {
+            System.out.println("Por favor, digite um número inteiro válido.");
+        }finally {
+            scanner.close();
         }
     }
 
-    /**
-     * @param parametroUm
-     * @param parametroDois
-     * @throws ParametrosInvalidosException
-     */
     static void contar (int parametroUm, int parametroDois) throws ParametrosInvalidosException {
         if (parametroDois <= parametroUm){
             throw new ParametrosInvalidosException();
